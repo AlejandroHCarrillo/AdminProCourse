f4b83fa8e265498083a73521a213d4c8c30ceda6
Alejandro <ahernandezdotnet@gmail.com>
2019-01-29 15:06:19 -0600

    Clean

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+    declarations:[
+        NopagefoundComponent,
+        HeaderComponent,
+        BreadcrumbsComponent,
+        SidebarComponent
+    ],
+    exports:[
+        NopagefoundComponent,
+        HeaderComponent,
+        BreadcrumbsComponent,
+        SidebarComponent
+    ]
+})
+export class SharedModule{};
